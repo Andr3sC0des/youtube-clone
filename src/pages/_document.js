b928b9032e8715ad3b034b171d01eb183af0cb49
andr3sc0des <andresqcodes@gmail.com>
2024-03-12 18:33:23 -0600

    Adding preconnect in metatags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document () {
   return (
     <Html className='html' lang='en'>
-      <Head />
+      <Head>
+        <link rel='preconnect' href='https://unavatar.io' />
+        <link rel='preconnect' href='https://i.ytimg.com' />
+        <link rel='preconnect' href='https://avatars.githubusercontent.com' />
+      </Head>
       <body>
         <Main />
         <NextScript />
