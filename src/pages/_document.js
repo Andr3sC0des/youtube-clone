a2b858cc2372ac4ea9a2425b465bd57906affb48
andr3sc0des <andresqcodes@gmail.com>
2024-03-12 18:41:39 -0600

    Only preconnect the ytimg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,9 @@ export default function Document () {
   return (
     <Html className='html' lang='en'>
       <Head>
-        <link rel='preconnect' href='https://unavatar.io' />
+        {/* <link rel='preconnect' href='https://unavatar.io' /> */}
         <link rel='preconnect' href='https://i.ytimg.com' />
-        <link rel='preconnect' href='https://avatars.githubusercontent.com' />
+        {/* <link rel='preconnect' href='https://avatars.githubusercontent.com' /> */}
       </Head>
       <body>
         <Main />
