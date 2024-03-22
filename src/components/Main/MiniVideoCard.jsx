74ea7cb64585b71100513f76993e7e0ecd06fd77
andr3sc0des <andresqcodes@gmail.com>
2024-03-22 03:29:32 -0600

    Removing skeleton and just using aspect ratio in minivideocard component

--- src/components/Main/MiniVideoCard.jsx
+++ src/components/Main/MiniVideoCard.jsx
@@ -1,19 +1,10 @@
-import ImageSkeleton from '../ImageSkeleton'
 import styles from './MiniVideoCard.module.sass'
 
 const MiniVideoCard = ({ id, title, channel, views, publishedDate }) => {
   return (
     <>
       <a href={`/watch?v=${id}`} className={styles.card}>
-        <ImageSkeleton
-          id={id}
-          aspectRatio='1.73'
-          customStyle={{ maxWidth: '168px', borderRadius: '1rem', overflow: 'hidden' }}
-          width='100%'
-          height='100%'
-          src={`https://i.ytimg.com/vi_webp/${id}/hqdefault.webp`}
-          alt={title}
-        />
+        <img className={styles.card__image} src={`https://i.ytimg.com/vi_webp/${id}/maxresdefault.webp`} alt={title} />
         <div className={styles.card__info}>
           <h2 className={styles.card__title}>{title.length > 42 ? `${title.slice(0, 42)}...` : title}</h2>
           <p className={styles.card__channel}>{channel}</p>
